9ab850b9ac6bdd41929567cfbf440a6ac558ad2b
Enmaaaaaa <83886600+Enmaaaaaa@users.noreply.github.com>
2024-11-20 15:00:25 -0500

    Update main_bm.cpp

--- benchmarks/main_bm.cpp
+++ benchmarks/main_bm.cpp
@@ -6,8 +6,10 @@
 #include "sequential_tree.h"
 #include "parellel_tree.h"
 
-static SequentialTree* arbol_datos = nullptr;
-static ParallelTree* arbol_datos = nullptr;
+// Variables para almacenar los árboles secuenciales y paralelos
+static SequentialTree* arbol_secuencial = nullptr;
+static ParallelTree* arbol_parallel = nullptr;
+
 static const int VALOR_MEDIO = 10;
 static const int NUMERO_ELEMENTOS = 5;
 static const int NUMERO_VECTORES = 50;
@@ -17,29 +19,42 @@ void inicializa() {
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> uni_dis(VALOR_MEDIO - 10, VALOR_MEDIO + 10);
 
+  // Inicializamos ambos árboles
   for(int i = 0; i < NUMERO_VECTORES; ++i) {
     std::vector<double> tmp(NUMERO_ELEMENTOS);
     for(int j = 0; j < NUMERO_ELEMENTOS; ++j) tmp[j] = uni_dis(gen);
 
-    if(arbol_datos == nullptr)
-      arbol_datos = new SequentialTree(tmp);
+    // Insertamos el vector en el árbol secuencial
+    if(arbol_secuencial == nullptr)
+      arbol_secuencial = new SequentialTree(tmp);
     else
-      arbol_datos->insert(tmp);
+      arbol_secuencial->insert(tmp);
+
+    // Insertamos el vector en el árbol paralelo
+    if(arbol_parallel == nullptr)
+      arbol_parallel = new ParallelTree(tmp);
+    else
+      arbol_parallel->insert(tmp);
   }
 }
 
-void finaliza() { delete arbol_datos; }
+void finaliza() { 
+  delete arbol_secuencial; 
+  delete arbol_parallel; 
+}
 
 static void BM_secuencial(benchmark::State& state) {
   for(auto _ : state) {
-    double res = arbol_datos->calculateMaxAverage();
+    // Usamos el árbol secuencial
+    double res = arbol_secuencial->calculateMaxAverage();
     benchmark::DoNotOptimize(res);
   }
 }
 
 static void BM_parallel(benchmark::State& state) {
   for(auto _ : state) {
-    double res = arbol_datos->calculateMaxAverage();
+    // Usamos el árbol paralelo
+    double res = arbol_parallel->calculateMaxAverage();
     benchmark::DoNotOptimize(res);
   }
 }
